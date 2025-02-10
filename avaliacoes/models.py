b3d1c67c360690a449c3b90b546e4cdc04cd0e13
Felipe Goncalves <felipeepires.95@gmail.com>
2025-02-10 20:38:45 -0300

    nova versao

--- avaliacoes/models.py
+++ avaliacoes/models.py
@@ -7,6 +7,7 @@ class Avaliacao(models.Model):
     comentario = models.TextField(null=True, blank=True)
     nota = models.DecimalField(max_digits=3, decimal_places=2)
     data = models.DateTimeField(auto_now_add=True)
+    observacao = models.TextField(null=True, blank=True)
 
     def __str__(self):
         return f'{self.nota} - {self.user.username}'
--- pontos_turisticos/settings.py
+++ pontos_turisticos/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['pontos-turisticos-16ed111bd97d.herokuapp.com', '127:0.0.1']
+ALLOWED_HOSTS = ['pontos-turisticos-16ed111bd97d.herokuapp.com', '127:0.0.1', 'localhost']
 
 
 # Application definition
